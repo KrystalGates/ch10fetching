41a3704605180a25cd112734674af003d48c4c9e
krystal gates <krystal_sabrina@yahoo.com>
2019-06-07 10:33:03 -0500

    completed exercise 2 fetching other people's data

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Practice: Displaying Foods
 // Create a DOM element in your index.html with a class of foodList.
 // Create a function which returns a string template. The template
 // is the HTML representation for a food item.
@@ -14,6 +15,7 @@
 //     });
 //   });
 
+// Practice: Fetching Other People's Data
 // Your job is to query the Open Food Facts API for each of your
 // products, and list the following additional information.
 // Ingredients
@@ -22,30 +24,48 @@
 // Fat per serving
 // Sugar per serving
 
+  let foodContainer = document.querySelector(".foodList")
+
+
+const foodFactory = food =>{
+  let foodAsHTML = `<div><h1>${food.name}</h1><p>Ingredients: ${food.ingredients}<p><p>Country of Origin: ${food.countries}</p><p>Calories: ${food.calories}</p><p>Fat per Serving: ${food.fat}</p><p>Sugar per Serving: ${food.sugar}</p></div>`
+  return foodAsHTML
+}
+
 fetch("http://localhost:8088/food")
-  .then(data => data.json())
-  .then(myParsedFoods => {
-    myParsedFoods.forEach(food => {
-      console.log(food.barcode);
-       // Now fetch the food from the Food API
-       fetch(`https://world.openfoodfacts.org/api/v0/product/${item.barcode}.json`)
-       .then(response => response.json())
-       .then(productInfo => {
-           food.ingredients = productInfo.product.ingredients
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food) // Should have a `barcode` property
 
-           // Produce HTML representation
-           let foodContainer = document.querySelector(".foodList")
-           const foodAsHTML = foodFactory(food) {
-               foodContainer.innerHTML += `<div><p>Ingredients: ${food.ingredients}</p></div>'
-           }
+            // Now fetch the food from the Food API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    food.ingredients = productInfo.product.ingredients_text
+                    food.countries = productInfo.product.countries
+                    food.calories = productInfo.product.nutriments.calories
+                    food.fat = productInfo.product.nutriments.fat
+                    food.sugar = productInfo.product.nutriments.sugars
+                    if (food.calories !== ""){
+                      food.calories = "Info not Available"
+                    }
+                    if(food.ingredients === ""){
+                      food.ingredients = "Info not Available"
+                    }
+                    else if(food.ingredients === undefined){
+                      food.ingredients = "Info not Available"
+                    }
+                    
 
-           // Add representaiton to DOM
-           addFoodToDom(foodAsHTML)
-       })
-})
-})
+                    console.log(food.ingredients)
 
+                    // Produce HTML representation
+                    const foodAsHTML = foodFactory(food)
 
- 
-
+                    // Add representaiton to DOM
+                    foodContainer.innerHTML+=foodAsHTML
+                })
+        })
+    })
 
